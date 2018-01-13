4ab05a9afca2c80289ae09f05f333ea50f336d09
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-01-13 01:09:24 -0600

    Firebase working, data printing to table.

--- assets/data.js
+++ assets/data.js
@@ -1,3 +1,9 @@
+$(document).ready(function() {
+var train;
+var destination;
+var arrival;
+var frequency;
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyCZYn12nR0s3NmYKrDg-4KgGhEJJyCpfSY",
@@ -8,6 +14,43 @@ var config = {
     messagingSenderId: "59693188757"
   };
   firebase.initializeApp(config);
+  console.log(firebase);
 
 // Create a variable to reference the database
-var database = firebase.database();
+var datab = firebase.database();
+
+console.log(datab);
+
+datab.ref().on("child_added", function(childSnapshot, prevChildKey) {
+  var trainVal = childSnapshot.val().name;
+  var destinationVal = childSnapshot.val().destination;
+  var arrivalVal = childSnapshot.val().arrival;
+  var frequencyVal = childSnapshot.val().frequency;
+  $("tbody").append("<tr><td>" + trainVal + "</td><td>" + destinationVal + "</td><td>" + arrivalVal + "</td><td>" + frequencyVal + "</td><td></td></tr>");
+
+
+  console.log(childSnapshot.val());
+})
+
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+  console.log("button clicked");
+  train = $("#trainName").val().trim();
+  destination = $("#destination").val().trim();
+  arrival = $("#arrival").val().trim();
+  frequency = $("#frequency").val().trim();
+
+  var object = {
+    name: train,
+    destination: destination,
+    arrival: arrival,
+    frequency: frequency
+  }
+  console.log(object);
+
+  datab.ref().push(object);
+
+
+})
+//datab.on("value", snap => console.log(snap.val()));
+})
