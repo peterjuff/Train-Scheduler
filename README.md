257a38275a4f5153b6352e4fadfe5ac80cc0666a
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-06-17 17:50:31 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Train-Scheduler
+
+A train schedule application that incorporates Firebase to host arrival and departure data.
+
+## Instructions
+
+* Look for the desired train to find departure/arrival times.
+* Add new train data and see real-time departure/arrival updates.
+
+![screenshot](assets/images/screenshot.png)
+
+## Tools and Software
+
+* [JQuery](https://jquery.com/)
+* [Bootstrap](https://getbootstrap.com/)
+* [Firebase](https://firebase.google.com/)
+
+# Authors
+
+* [Peter Juffernbruch](https://github.com/peterjuff)
+
+# Acknowledgments
+
+* [Northwestern University Coding Bootcamp](https://bootcamp.northwestern.edu/coding/)
+* Homework week seven
--- assets/data.js
+++ assets/data.js
@@ -81,28 +81,6 @@ $(document).ready(function() {
     console.log(nextArr);
 
 
-  
-
-    // //arrivalVal + frequencyVal is next train
-    // var momentArrive = moment(arrivalVal, 'HH:mm').subtract(1, 'years').add(frequencyVal, 'minutes').format('hh:mm');  
-    // console.log(momentArrive);
-
-    // //diff between cuurent time and train arrival in minutes is minaway
-    // var currentTime = moment();
-    // var timeDiff = moment.utc(moment(momentArrive, 'HH:mm').diff(moment(currentTime, "HH:mm"))).format("HH:mm");
-    // var toMinutes = moment.duration(timeDiff).asMinutes();
-    // console.log(timeDiff);
-    // console.log(toMinutes);
-    // var minAway = toMinutes;
-
-    // var timeLeft = timeDiff % minAway;
-    // console.log(timeLeft)
-
-    // var minutesAway = frequencyVal - timeLeft;
-    // console.log(minutesAway);
-    // console.log(toMinutes);
-    // console.log(minAway);
-
     $("tbody").append("<tr><td>" + trainVal + "</td><td>" + destinationVal + "</td><td>" + arrivalVal + "</td><td>" + frequencyVal + "</td><td>" + nextArr + "</td></tr>");
 
   });
@@ -112,64 +90,3 @@ $(document).ready(function() {
 });
 
 
-
-// $(document).ready(function() {
-// var train;
-// var destination;
-// var arrival;
-// var frequency;
-// var away;
-
-
-// // Initialize Firebase
-// var config = {
-//     apiKey: "AIzaSyCZYn12nR0s3NmYKrDg-4KgGhEJJyCpfSY",
-//     authDomain: "train-scheduler-a4fe3.firebaseapp.com",
-//     databaseURL: "https://train-scheduler-a4fe3.firebaseio.com",
-//     projectId: "train-scheduler-a4fe3",
-//     storageBucket: "train-scheduler-a4fe3.appspot.com",
-//     messagingSenderId: "59693188757"
-//   };
-//   firebase.initializeApp(config);
-//   console.log(firebase);
-
-// var datab = firebase.database();
-
-// console.log(datab);
-
-// var time = moment();
-
-// datab.ref().on("child_added", function(childSnapshot, prevChildKey) {
-//   var trainVal = childSnapshot.val().name;
-//   var destinationVal = childSnapshot.val().destination;
-//   var arrivalVal = childSnapshot.val().arrival;
-//   var frequencyVal = childSnapshot.val().frequency;
-//   var minutesAway = childSnapshot.val().away;
-//   $("tbody").append("<tr><td>" + trainVal + "</td><td>" + destinationVal + "</td><td>" + arrivalVal + "</td><td>" + frequencyVal + "</td><td>" + minutesAway + "</td></tr>");
-
-
-//   console.log(childSnapshot.val());
-// })
-
-// $("#submit").on("click", function(event) {
-//   event.preventDefault();
-//   console.log("button clicked");
-//   train = $("#trainName").val().trim();
-//   destination = $("#destination").val().trim();
-//   arrival = $("#arrival").val().trim();
-//   frequency = $("#frequency").val().trim();
-
-//   var object = {
-//     name: train,
-//     destination: destination,
-//     arrival: arrival,
-//     frequency: frequency
-//   }
-//   console.log(object);
-
-//   datab.ref().push(object);
-
-
-// })
-
-// })
